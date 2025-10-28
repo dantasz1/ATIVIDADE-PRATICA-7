f1698f743a302114619c2e8b2feba374f2c547f3
Andre <dantasluizandre@gmail.com>
2025-10-28 12:22:41 -0300

    first commit

---
+++ src/main/java/br/edu/fatecpg/model/Pedido.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.model;
+
+public class Pedido {
+
+    private String nome;
+    private String email;
+    private String quantidadePedido;
+    private int idPedido;
+    
+
+}
+
+
---
+++ src/main/java/br/edu/fatecpg/model/Produto.java
@@ -0,0 +1,67 @@
+package br.edu.fatecpg.model;
+
+import java.util.Objects;
+
+public class Produto {
+
+    private String id;
+    private String nome;
+    private String preco;
+    private int quantidade;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", preco='" + preco + '\'' +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return quantidade == produto.quantidade && Objects.equals(id, produto.id) && Objects.equals(nome, produto.nome) && Objects.equals(preco, produto.preco);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, preco, quantidade);
+    }
+
+}
